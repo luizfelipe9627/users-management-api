6fcc05611650f73eb6789d447c978fb5e0c0bb9a
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-07-24 20:32:21 -0300

    feat: criado configuracao principal do express.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const router = require("./routes/router")
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+module.exports = app;
