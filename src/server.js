bd193c37d2c1019a92b1fddcee1a4c8cb84f3876
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-07-24 20:32:40 -0300

    feat: criado servidor responsavel por rodar o app.

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const app = require("./app");
+const swaggerDocument = require("../swagger.json");
+const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  () => console.log(`Servidor rodando em http://localhost:${PORT}`),
+);
+
+const options = {
+  customCss: ".swagger-ui .topbar { display: none }",
+};
+
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options),
+);
